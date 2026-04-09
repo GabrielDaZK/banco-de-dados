f1049de117b1a30265e203c01773e57d00953cc7
GabrielDaZK <gabriel.antunes.correa@escola.pr.gov.br>
2026-04-09 10:28:39 -0300

    Create Clientes and Compras tables in SQL

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -1,1 +1,15 @@
+-- Criação da tabela Clientes
+-- Criar tabela Clientes
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY,
+    nomeCliente VARCHAR(100) NOT NULL,
+    emailCliente VARCHAR(100) UNIQUE NOT NULL
+);
 
+-- Criar tabela Compras
+CREATE TABLE Compras (
+    CompraID INT PRIMARY KEY,
+    ClienteID INT,
+    NomeLivro VARCHAR(200) NOT NULL,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
+);
