54613ef4db35f374743c0a0f382338cfbce4943d
GabrielDaZK <gabriel.antunes.correa@escola.pr.gov.br>
2026-04-09 10:33:38 -0300

    Update sistema-livraria.sql

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -1,15 +1,15 @@
--- Criação da tabela Clientes
--- Criar tabela Clientes
-CREATE TABLE Clientes (
-    ID INT PRIMARY KEY,
-    nomeCliente VARCHAR(100) NOT NULL,
-    emailCliente VARCHAR(100) UNIQUE NOT NULL
-);
+-- Inserir dados na tabela Clientes
+INSERT INTO Clientes (ID, nomeCliente, emailCliente) exercicos 6
+VALUES
+(1, 'Gabriel Souza', 'gabriel.souza@email.com'),
+(2, 'Maria Oliveira', 'maria.oliveira@email.com'),
+(3, 'João Pereira', 'joao.pereira@email.com');
 
--- Criar tabela Compras
-CREATE TABLE Compras (
-    CompraID INT PRIMARY KEY,
-    ClienteID INT,
-    NomeLivro VARCHAR(200) NOT NULL,
-    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
-);
+-- Inserir dados na tabela Compras
+INSERT INTO Compras (CompraID, ClienteID, NomeLivro)
+VALUES
+(101, 1, 'Dom Casmurro'),
+(102, 1, 'O Senhor dos Anéis'),
+(103, 2, 'A Culpa é das Estrelas'),
+(104, 3, '1984'),
+(105, 3, 'O Pequeno Príncipe');
